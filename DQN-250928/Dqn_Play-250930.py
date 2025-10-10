e577d1a5c334c078aed254877b0769a7ca8facf2
jk049 <jk049jk@gmail.com>
2025-10-10 09:13:43 +0800

    Breakout double dueling pth and video update, score 350

--- DQN-250928/Dqn_Play-250930.py
+++ DQN-250928/Dqn_Play-250930.py
@@ -12,22 +12,21 @@ from lib import model
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--model_dir', type=str, default='./out/', help='directory to save the model, default ./out/')
     parser.add_argument('-m', '--model', type=str, default='dqn.pth', help='path to the trained model, default dqn.pth')
-    parser.add_argument('-e', '--env', type=str, default='PongNoFrameskip-v4', help='environment name, default PongNoFrameskip-v4')
     parser.add_argument('-r', '--record', default='./record_video/', help='directory to save the video, default not to record')
     parser.add_argument('-v', '--visible', action='store_true', help='whether to render the environment')
 
     args = parser.parse_args()
-    env = env_wrappers.make_env(args.env, render_mode="human" if args.visible else None)
+    env = env_wrappers.make_env(args.model.split('_')[0], render_mode="human" if args.visible else None)
     if args.record:
-        env = gym.wrappers.RecordVideo(env, video_folder=args.record+args.env, episode_trigger=lambda x: True)
-        #env = gym.wrappers.Monitor(env, args.record)
+        env = gym.wrappers.RecordVideo(env, video_folder=args.record+args.model.split('.')[0], episode_trigger=lambda x: True)
     
     if 'dueling' in args.model:
         net = model.Dueling_Net(env.observation_space.shape, env.action_space.n)
     else:
         net = model.Q_Net(env.observation_space.shape, env.action_space.n)
-    net.load_state_dict(torch.load(args.model, map_location=lambda storage, loc: storage, weights_only=True))
+    net.load_state_dict(torch.load(args.model_dir+args.model, map_location=lambda storage, loc: storage, weights_only=True))
     net.eval()
     state = env.reset()
     done = False
